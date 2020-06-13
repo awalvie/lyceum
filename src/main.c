89e471b2b2c2c9a63bbd193d7510e89cc9820a97
awalvie <agrawal.vishesh.178@gmail.com>
2020-06-13 18:23:21 +0530

    The data returned by readdir() may be overwritten by subsequent calls to readdir() for the same directory stream. list->files[list->number_of_files] pointed to a temporary structure, now it copies the file name into a heap alloacted memory that is later freed

--- src/main.c
+++ src/main.c
@@ -1,4 +1,16 @@
+/* necessary to use strdup */
+#ifdef __STDC_ALLOC_LIB__
+#define __STDC_WANT_LIB_EXT2__ 1
+#else
+#define _POSIX_C_SOURCE 200809L
+#endif
+
+#define NAME_BUFFER 50
+#define DESCRIPTION_BUFFER 256
+#define CONTENT_BUFFER 8192
+
 #include "helper.c"
+#include <assert.h>
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,10 +18,6 @@
 #include <unistd.h>
 
 #define FILE_BUFFER 1024
-#define DESCRIPTION_BUFFER 200
-#define PAGE_NAME_BUFFER 100
-#define FILE_PATH 256
-#define CONTENT_BUFFER 8192
 
 char *html_head =
     "<html lang='en'>\n"
@@ -35,10 +43,10 @@ typedef struct {
 
 /* Structure for parsing the contents of the page */
 typedef struct {
-  char *name;
-  char *description;
-  char *content;
-  long date;
+  char name[NAME_BUFFER];
+  char description[DESCRIPTION_BUFFER];
+  char content[CONTENT_BUFFER];
+  Date date;
 } Page;
 
 /* fetch all the the files in a specificed directory into a FileList structure
@@ -55,57 +63,124 @@ Date to_date(long unformatted) {
 
 /* Structure for getting list of files in a directory */
 typedef struct {
-  char *files[FILE_PATH];
+  char *files[FILENAME_MAX];
   int number_of_files;
 } FileList;
 
-Page createPage() {
-  Page page;
-  page.name = NULL;
-  page.description = NULL;
-  page.content = NULL;
-  page.date = 0;
-  return page;
-}
-
 /* return all file names in a directory in a array of strings */
-void getFileNames(char *name, FileList *list) {
+void getFileNames(const char *name, FileList *list) {
   DIR *dir;
-  struct dirent *ent;
-  list->number_of_files = 0;
-  if ((dir = opendir(name)) != NULL) {
-    while ((ent = readdir(dir)) != NULL) {
-      if (ent->d_name[0] != '.') {
-        list->files[list->number_of_files] = ent->d_name;
-        list->number_of_files++;
-      }
+  struct dirent *entry;
+
+  dir = opendir(name);
+  if (dir == NULL) {
+    perror("opendir");
+    exit(-1);
+  }
+
+  while ((entry = readdir(dir))) {
+    if ((entry->d_name[0] != '.')) {
+      char *name = strdup(entry->d_name);
+      list->files[list->number_of_files] = name;
+      list->number_of_files++;
     }
   }
   closedir(dir);
 }
 
-void parseFile(const char *file_name) {
-  char *name;
-  chdir("../content/");
-  FILE *fp;
-  if ((fp = fopen(file_name, "r")) == NULL) {
-    printf("could not open file %s\n", file_name);
-  }
-  fclose(fp);
-}
-
-void buildSite(FileList list) {
+void freeFileList(FileList list) {
   int i;
   for (i = 0; i < list.number_of_files; i++) {
-    parseFile(list.files[i]);
+    free(list.files[i]);
+  }
+}
+
+Page newPage() {
+  Page page;
+  memset(page.name, 0, sizeof(page.name));
+  memset(page.description, 0, sizeof(page.description));
+  memset(page.content, 0, sizeof(page.content));
+  return page;
+}
+
+FileList new_list() {
+  FileList list;
+  list.number_of_files = 0;
+  return list;
+}
+
+Page parseFile(char *file_name) {
+  Page page;
+  long date;
+  char *full_path = concat("../content/", file_name);
+  FILE *fp;
+  if ((fp = fopen(full_path, "r")) == NULL) {
+    printf("could not open file %s\n", full_path);
+  }
+  /* get name from the first line */
+  fgets(page.name, NAME_BUFFER, fp);
+  strtok(page.name, "\n");
+
+  /* get description from the second line */
+  fgets(page.description, DESCRIPTION_BUFFER, fp);
+  strtok(page.description, "\n");
+
+  /* get date from third line */
+  fscanf(fp, "%ld", &date);
+  page.date = to_date(date);
+
+  int c;
+  int j = 0;
+  while ((c = fgetc(fp)) != EOF) {
+    page.content[j] = c;
+    j++;
+  }
+
+  free(full_path);
+  fclose(fp);
+  return page;
+}
+
+void removeFileExtension(char *file_name) {
+
+  int j = 0;
+  while (file_name[j] != '.') {
+    j++;
+  }
+  file_name[j] = '\0';
+}
+
+void buildSite(FileList *list) {
+  int i;
+  for (i = 0; i < list->number_of_files; i++) {
+
+    Page page;
+    page = parseFile(list->files[i]);
+    /* remove the .txt extension from the file name */
+    char *file_name = list->files[i];
+    removeFileExtension(file_name);
+    /* full path of the final file */
+    char *full_path = concat(concat("../docs/", file_name), ".html");
+    puts(full_path);
+
+    FILE *fp;
+    fp = fopen(full_path, "w");
+    fputs(html_head, fp);
+    fputs(html_header, fp);
+    fputs(page.content, fp);
+    fputs(html_footer, fp);
+
+    fclose(fp);
+    free(full_path);
   }
 }
 
 int main(void) {
 
-  FileList list;
-  getFileNames("../content", &list);
-  buildSite(list);
+  FileList list = new_list();
+  getFileNames("../content/", &list);
+  buildSite(&list);
+  freeFileList(list);
 
   return 0;
 }
