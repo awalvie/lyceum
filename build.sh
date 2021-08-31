9e50c871e4b167bcdffdabc6a10ae1b79b06855b
awalvie <agrawal.vishesh.178@gmail.com>
2021-08-31 09:36:45 +0530

    feat: add build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+rm -rf go-blender
+git clone https://github.com/awalvie/go-blender
+cd go-blender && make && cd ..
+mv go-blender/go-blender ./blender
+
+if [[ -e ./build ]];
+	then rm -r ./build
+fi
+
+mkdir build
+./blender build ./
