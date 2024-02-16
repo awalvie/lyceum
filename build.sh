1de2e8c2b96c7c532c6d6ae626bd72684684c35b
awalvie <agrawal.vishesh.178@gmail.com>
2024-02-16 15:42:00 +0100

    build: fix failures

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-#!/bin/sh -x
+#!/bin/bash -x
 
 rm -rf go-blender
 git clone https://github.com/awalvie/go-blender
-cd go-blender && make && cd ..
+cd go-blender && make build && cd ..
 mv go-blender/go-blender ./blender
 
 if [[ -e ./build ]];
